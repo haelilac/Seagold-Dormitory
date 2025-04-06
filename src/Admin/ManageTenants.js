dfbe09564c772046604b0de0b42ba4dadfa0520c
haelilac <shanacarmellabolok@gmail.com>
2025-04-06 10:01:40 +0800

    css

--- src/Admin/ManageTenants.js
+++ src/Admin/ManageTenants.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './ManageTenants.css';
 
 const ManageTenants = () => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
--- src/Admin/PendingApplications.js
+++ src/Admin/PendingApplications.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './PendingApplications.css'; // Add custom styles here
+
 
 const PendingApplications = () => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
