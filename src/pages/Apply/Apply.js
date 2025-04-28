3be2b8ec26d8b5121b43bc13dba5fa0cbeafe7d7
haelilac <shanacarmellabolok@gmail.com>
2025-04-28 22:05:26 +0800

    Update Apply.js

--- src/pages/Apply/Apply.js
+++ src/pages/Apply/Apply.js
@@ -256,7 +256,7 @@ const ContactUs = () => {
         formDataUpload.append("receipt", file);
     
         try {
-            const response = await fetch("https://seagold-python-production.up.railway.app/validate-receipt", {
+            const response = await fetch("https://seagold-python-production.up.railway.app/api/validate-receipt", {
                 method: "POST",
                 body: formDataUpload,
             });
@@ -287,7 +287,7 @@ const ContactUs = () => {
         formDataUpload.append("id_type", formData.id_type);
     
         try {
-            const response = await fetch("https://seagold-python-production.up.railway.app/upload-id", { 
+            const response = await fetch("https://seagold-python-production.up.railway.app/api/upload-id", { 
                 method: 'POST',
                 body: formDataUpload,
                 headers: { 
